1122c6cccf348b2f603f9462fc27a5f0d2e3a6f7
Kakmusaya <scadaengineercs@gmail.com>
2024-09-23 17:13:09 +0500

    add background color, and text

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(MaterialApp(home: MyApp()));
+  runApp(const MaterialApp(home: MyApp()));
 }
 
+List<int> number = [1, 2, 3];
+
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Scaffold(
+      body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 26, 2, 80),
+                Color.fromARGB(255, 45, 7, 98),
+              ],
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              "Salem Kazakhstan!",
+              style: TextStyle(color: Colors.white, fontSize: 30),
+            ),
+          )),
+    );
   }
 }
